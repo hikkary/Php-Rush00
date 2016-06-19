60c435257ec7b24d07ed6392202883ecbd69793e
Zakaria KERKEB <zkerkeb@e3r8p17.42.fr>
2016-06-19 15:44:57 +0200

    Apercu categorie ok

--- fct/display.php
+++ fct/display.php
@@ -44,4 +44,33 @@ function group($group)
 			}
 }
 
+function group2($group)
+{
+	if(!$group)
+		$group = "Pokemon";
+		$p = file_get_contents('json/products.json');
+		$p = json_decode($p, true);
+		$i = 0;
+			foreach ($p as $product)
+			{
+				if((stripos($product['group'],$group)) !== FALSE && $i < 2)
+				{
+
+					echo "<img src='{$product['image']}'/>" ."<br>";
+					echo($product['name']."<br>");
+					echo($product['price']."$"."<br>"."<br>");
+					echo ' <form  action="add_panier.php" method="post">'."<br>";
+					echo '<input type="number" name="qty" value="1">';
+					echo"<input type='submit' name='ajout' value='{$product[name]}' placeholder='Name'>"."<br>";
+					echo($product['description']."<br>");
+					echo ' </form>';
+					$i++;
+			}
+			}
+
+}
+
+
+
+
 ?>
--- pokemon.php
+++ pokemon.php
@@ -27,6 +27,72 @@ session_start();
 		</div>
 </div>
 
+<div class="poketop">
+	<a href="#">
+		<h1 class="poketitre">Nos Derniers Pokemon Eau</h1>
+	</a>
+		<div class="affichage">
+		<?php
+			$rootname = getcwd();
+			require_once($rootname.'/fct/display.php');
+			group2('Eau');
+		?>
+		</div>
+</div>
+
+<div class="poketop">
+	<a href="#">
+		<h1 class="poketitre">Nos Derniers Pokemon Plante</h1>
+	</a>
+		<div class="affichage">
+		<?php
+			$rootname = getcwd();
+			require_once($rootname.'/fct/display.php');
+			group2('Plante');
+		?>
+		</div>
+</div>
+
+<div class="poketop">
+	<a href="#">
+		<h1 class="poketitre">Nos Derniers Pokemon Pierre</h1>
+	</a>
+		<div class="affichage">
+		<?php
+			$rootname = getcwd();
+			require_once($rootname.'/fct/display.php');
+			group2('Pierre');
+		?>
+		</div>
+</div>
+
+<div class="poketop">
+	<a href="#">
+		<h1 class="poketitre">Nos Derniers Pokemon Poison</h1>
+	</a>
+		<div class="affichage">
+		<?php
+			$rootname = getcwd();
+			require_once($rootname.'/fct/display.php');
+			group2('Poison');
+		?>
+		</div>
+</div>
+
+<div class="poketop">
+	<a href="#">
+		<h1 class="poketitre">Nos Derniers Pokemon Legendaire</h1>
+	</a>
+		<div class="affichage">
+		<?php
+			$rootname = getcwd();
+			require_once($rootname.'/fct/display.php');
+			group2('Pokemon Legendaire');
+		?>
+		</div>
+</div>
+
+
 
 </body>
 </html>
